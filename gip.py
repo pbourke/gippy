95631c5699f59b5737b78172ce6b742bfb180661
Patrick Bourke <pbourke@gmail.com>
2011-11-24 17:22:57 -0800

    Successfully added a message to Notes folder via the add_test_note method - working on making node user-editable

--- gip.py
+++ gip.py
@@ -17,6 +17,13 @@ import argparse
 import imaplib
 import pprint
 import email
+import email.message
+import email.utils
+import datetime
+import time
+import uuid
+import os
+import tempfile
 
 def get_args():
     """Read credentials from command line arguments"""
@@ -63,10 +70,51 @@ def list_notes(args):
         msg = fetch_message(imap, imap_id)
         print(imap_id, msg['Date'], msg['Subject'])
 
+def now_in_rfc_format():
+    """http://stackoverflow.com/questions/3453177/convert-python-datetime-to-rfc-2822"""
+    nowdt = datetime.datetime.now()
+    nowtuple = nowdt.timetuple()
+    nowtimestamp = time.mktime(nowtuple)
+    return email.utils.formatdate(nowtimestamp)
+
+def add_test_note(args):
+    filename = ""
+    with tempfile.TemporaryFile() as f:
+        f.write("Subject: edit me\r\n")
+        filename = f.name
+
+    # launch $EDITOR to create the note
+    edit_cmd = "vi"
+    if 'EDITOR' in os.environ:
+        edit_cmd = os.environ['EDITOR']        
+
+    os.system(edit_cmd + " " + filename)
+
+    # format message
+    now = now_in_rfc_format() 
+
+    msg = email.message.Message()
+    msg['Subject'] = "Another test!"
+    msg['From'] = args.username
+    msg['To'] = args.username
+    msg['Content-Type'] = "text/html; charset=utf-8"
+    msg['X-Uniform-Type-Identifier'] = "com.apple.mail-note"
+    msg['X-Mail-Created-Date'] = now
+    msg['Date'] = now
+    msg['X-Universally-Unique-Identifier'] = str(uuid.uuid4()).upper()
+    # todo: read in tempfile and set its content as payload
+    msg.set_payload("<div>This is a test from Python</div><div><b>bold</b></div><div><ul><li>item 1</li><li>item 2</li></div>")
+
+    # imap append
+    imap = connect_to_imap_server(args)
+    imap.append('Notes', '', imaplib.Time2Internaldate(time.time()), str.encode(str(msg)))
+
 if __name__ == "__main__":
     args = get_args()
     if args.action == 'list':
         list_notes(args)
     elif args.action == 'show':
         show_note(args)
+    elif args.action == 'test-add':
+        add_test_note(args)
 
